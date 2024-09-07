45986ec78b8535f3aac1decd02be20fbad3fb907
akbaroke <akbarmuhammad833@gmail.com>
2024-09-07 12:15:35 +0700

    update: test implement pwa

--- src/helpers/getIramaAdzan.ts
+++ src/helpers/getIramaAdzan.ts
@@ -1,18 +1,14 @@
-import ADZAN_JIHARKAH from '@/assets/audio/adzan_jiharkah.mp3';
-import ADZAN_KURDI from '@/assets/audio/adzan_kurdi.mp3';
-import ADZAN_MEKKAH from '@/assets/audio/adzan_mekkah.mp3';
-
 export default function getIramaAdzan(irama: string) {
   let adzan;
   switch (irama) {
     case 'jiharkah':
-      adzan = ADZAN_JIHARKAH;
+      adzan = '/audio/adzan_jiharkah.mp3';
       break;
     case 'kurdi':
-      adzan = ADZAN_KURDI;
+      adzan = '/audio/adzan_kurdi.mp3';
       break;
     default:
-      adzan = ADZAN_MEKKAH;
+      adzan = '/audio/adzan_mekkah.mp3';
       break;
   }
 
