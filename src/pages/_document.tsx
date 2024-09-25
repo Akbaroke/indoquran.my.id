1858ad7212539bd81359fe6b51d9afe0faa19b70
akbaroke <akbarmuhammad833@gmail.com>
2024-09-25 12:05:43 +0700

    fix(pwa): update manifest dan aset pwa

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,6 +5,17 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta name="format-detection" content="telephone=no" />
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="theme-color" content="#FFFFFF" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/icons/apple-touch-icon.png"
+        />
         <link rel="manifest" href="/manifest.json" />
         <link rel="shortcut icon" href="/favicon.ico" />
         <ColorSchemeScript defaultColorScheme="light" />
